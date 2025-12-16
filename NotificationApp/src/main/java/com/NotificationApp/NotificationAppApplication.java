c6a0bf9f665cf386fe6077d6be02e73aeaeb0c05
Govind Bansal <bansalgovind2003@gmail.com>
2025-12-16 14:37:20 +0530

    ADDED ELK

--- NotificationApp/src/main/java/com/NotificationApp/NotificationAppApplication.java
+++ NotificationApp/src/main/java/com/NotificationApp/NotificationAppApplication.java
@@ -1,12 +1,15 @@
 package com.NotificationApp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class NotificationAppApplication {
 
 	public static void main(String[] args) {
+		log.info("Starting Notification Application...");
 		SpringApplication.run(NotificationAppApplication.class, args);
 	}
 
--- notification-worker/src/main/java/com/notification_worker/NotificationWorkerApplication.java
+++ notification-worker/src/main/java/com/notification_worker/NotificationWorkerApplication.java
@@ -1,12 +1,15 @@
 package com.notification_worker;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class NotificationWorkerApplication {
 
 	public static void main(String[] args) {
+		log.info("Starting Worker Application...");
 		SpringApplication.run(NotificationWorkerApplication.class, args);
 	}
 
